bcb38d85be9feb8af1c0064a2715fc1c36608585
Sina Shabani <sina.sh.k78@gmail.com>
2019-03-28 18:11:47 +0430

    responsive design half way done.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -19,11 +19,11 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends Application {
     private static String searchWord = "";
@@ -37,6 +37,10 @@ public class Main extends Application {
     private static Button btnSearch;
     private static Button btnSave ;
     private static TextField txtSearchWord ;
+    private static HBox toolBar;
+    private static HBox mainBox;
+    private static double WINDOW_WIDTH = 1600;
+    private static double WINDOW_HEIGHT = 900;
 
     @Override
    public void start(Stage primaryStage)throws Exception
@@ -46,35 +50,38 @@ public class Main extends Application {
        //left side of the page
        foundGridPane = new GridPane();
        foundScrollPane = new ScrollPane(foundGridPane);
-       foundScrollPane.setPrefWidth(1300);
+       foundScrollPane.setPrefWidth((3*WINDOW_WIDTH)/4);
 
        //right side of the page
        savedResults = new VBox();
        savedScrolledPane =  new ScrollPane(savedResults);
-       savedScrolledPane.setPrefWidth(300);
+       savedScrolledPane.setPrefWidth(WINDOW_WIDTH/4);
        //main Layout
-       HBox mainBox =new HBox(foundScrollPane , savedScrolledPane);
+       mainBox =new HBox(foundScrollPane , savedScrolledPane);
        BorderPane root = new BorderPane(mainBox);
 
-       //setting alerts
-//       Alert alert = new Alert(Alert.AlertType.WARNING);
-//       alert.setTitle("Warning");
-//       alert.setHeaderText(null);
        //setting up the controls.
        pb = new ProgressBar();
-       pb.setPrefSize(500 , 30);
+       //pb.setPrefSize(500 , 30);
        btnSearch = new Button("Search");
        btnSave = new Button("Save");
-       txtSearchWord = new TextField() ;
+       txtSearchWord = new TextField();
        btnSearch.setDisable(true);
        btnSave.setDisable(true);
        //adding the toolbar.
-       HBox toolBar = new HBox();
+       toolBar = new HBox();
        toolBar.getChildren().addAll(txtSearchWord,btnSearch, btnSave , pb);
+        //TODO fix this shit
+       txtSearchWord.setPrefHeight(toolBar.getHeight());
+       txtSearchWord.setPrefWidth(WINDOW_WIDTH/3);
+       btnSave.setPrefHeight(toolBar.getHeight());
+       btnSave.setPrefHeight(toolBar.getHeight());
+       pb.setPrefWidth(WINDOW_WIDTH - (btnSave.getWidth() + btnSearch.getWidth() + txtSearchWord.getWidth()));
+       pb.setPrefHeight(toolBar.getHeight());
+
        root.setLayoutX(0);
        root.setLayoutY(0);
        root.setBottom(toolBar);
-
        //Load the current searched words
        loadSavedResults();
 
@@ -89,8 +96,6 @@ public class Main extends Application {
 
            }
        });
-
-
        btnSearch.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
@@ -118,8 +123,6 @@ public class Main extends Application {
                 }
             }
         });
-
-
        btnSave.setOnAction(new EventHandler<ActionEvent>() {
            @Override
            public void handle(ActionEvent actionEvent) {
@@ -142,7 +145,17 @@ public class Main extends Application {
            }
        });
 
-       Scene scene = new Scene(root, 1600 , 900);
+       primaryStage.widthProperty().addListener( (observableValue, number, t1) -> {
+           txtSearchWord.setPrefHeight(toolBar.getHeight());
+           System.out.println(toolBar.getHeight() + " txt: " + txtSearchWord.getHeight());
+           WINDOW_WIDTH = (double)t1;
+           foundScrollPane.setPrefWidth((3*WINDOW_WIDTH)/4);
+           savedScrolledPane.setPrefWidth(WINDOW_WIDTH/4);
+
+       });
+
+
+       Scene scene = new Scene(root, WINDOW_WIDTH , WINDOW_HEIGHT);
        primaryStage.setScene(scene);
        primaryStage.setTitle("IMDB");
        primaryStage.show();
@@ -175,7 +188,7 @@ public class Main extends Application {
            });
            imageView = new ImageView(image);
            links.add("https://www.imdb.com/"+ title.select("td:nth-of-type(2) a").attr("href"));
-           link.setOnAction(actionEvent1 -> getHostServices().showDocument("https://www.imdb.com/"+ title.select("td:nth-of-type(2) a").attr("href")));
+           link.setOnAction(actionEvent1 -> getHostServices().showDocument("https://www.imdb.com"+ title.select("td:nth-of-type(2) a").attr("href")));
            foundGridPane.addRow(i++,imageView, link);
 
        }
@@ -205,7 +218,7 @@ public class Main extends Application {
     private void loadSavedResults()
     {
         savedResults.getChildren().clear();
-        for(String str : load())
+        for(String str : getWords())
         {
             Hyperlink h = new Hyperlink(str);
             h.setOnAction(actionEvent -> loadData(str));
@@ -220,28 +233,34 @@ public class Main extends Application {
 
 
         try{
+            //creating the Folder structure for the current searchWord that we want to save
             File f = new File("Resources/"+searchWord);
             File imageFolder= new File(f.getPath() + "/Images");
             f.mkdirs();
             imageFolder.mkdirs();
+
             File imageFile;
             BufferedImage bImage;
             File result = new File(f.getPath() + "/result.txt");
+
             FileWriter fr = new FileWriter(result, true);
             BufferedWriter br = new BufferedWriter(fr);
             PrintWriter pr = new PrintWriter(br);
+
             int i = 1 , j = 0;
+
             for (Node node : nodes) {
                 if(node instanceof ImageView)
                 {
                     imageFile = new File(imageFolder.getPath()+ "/" + i++ + ".jpg");
                     bImage = SwingFXUtils.fromFXImage(((ImageView) node).getImage() , null);
-                    ImageIO.write(bImage , "jpg" , imageFile);
-                }else if (node instanceof Hyperlink){
-                    pr.println(((Hyperlink)node).getText() +"$"+ links.get(j++) +"$");
-                }
+                    ImageIO.write(bImage , "jpg" , imageFile); //saving all images to "searchWord/Image" folder
+
+                }else if (node instanceof Hyperlink)
+                    pr.println(((Hyperlink)node).getText() +"$"+ links.get(j++) +"$"); //saving the titles and links to result.txt
 
             }
+
             pr.flush();
             pr.close();
             br.close();
@@ -256,8 +275,14 @@ public class Main extends Application {
     }
 
 
+    /**
+     * this method will check if we have searched for a word before or not
+     *
+     * */
+
 
     private boolean exists() {
+
         try{
             File f = new File("Resources");
             String[] dirs = f.list();
@@ -272,7 +297,11 @@ public class Main extends Application {
 
     }
 
-    private String[] load()
+    /**
+     * this will return all the words the user searched for
+     *
+     * */
+    private String[] getWords()
     {
         File f = new File("Resources/");
         return f.list();
